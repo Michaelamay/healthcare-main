9241169037bd556664720b092b4ae35a301de623
Michaelamay <michaelamay1@yahoo.com>
2026-01-18 15:18:34 -0600

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+
+
+//import { PasskeyModal } from "@/components/PasskeyModal";
+//LEFT OFF LINE 12.
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-white text-3xl underline">Home 2</h1>
+      <Button>Click me</Button>
+      
+    </div>
+  )
+};
+
+//export default Home;
