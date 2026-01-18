c333b7b5aba6baa3874c6da4b9ba1d64ff48a8f8
Michaelamay <michaelamay1@yahoo.com>
2026-01-18 16:35:16 -0600

    Added images changed main page created form

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,37 @@
 import Image from "next/image";
-import Link from "next/link";
-import {Button} from "@/components/ui/button";
+import healthLogo from '@/public/assets/icons/health-logo.svg';
+import medicalStethoscope from '@/public/assets/images/medical-banner-with-stethoscope.jpg'
+import PatientForm from "@/components/forms/PatientForm";
 
-
-//import { PasskeyModal } from "@/components/PasskeyModal";
-//LEFT OFF LINE 12.
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home 2</h1>
-      <Button>Click me</Button>
-      
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={healthLogo}
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />  
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+            © Michaels Healthcare App Management System Demo
+            </p>
+         
+          </div>
+        </div>
+      </section>
+      <Image
+        src={medicalStethoscope}
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />    
     </div>
   )
 };
---
+++ components/forms/PatientForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PatientForm = () => {
+    return (
+        <div>PatientForms</div>
+    )
+}
+
+export default PatientForm
